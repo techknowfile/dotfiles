36ca1814189b80858ce1cf848e77ac0c399b311d
Andrew Dudley <addudley@asu.edu>
2019-04-18 17:03:21 -0700

    HiDPI + External screenlayout

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1-2 --off --output eDP-1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1-1 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-1-1 --off --output DP-1-2 --off
